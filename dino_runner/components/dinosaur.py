789f297397afe51dac19d59c59e2bea3879a1a01
CaptainJKM <edgar.vicente@fundacion-jala.org>
2023-03-06 21:01:39 -0600

    Class 3: Obstacles

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -21,6 +21,7 @@ class Dinosaur(Sprite):
         self.jump_vel = self.JUMP_VEL
 
     def update(self, user_input):
+        # Llama a los metodos para hacer las modificaciones de Animacion
         if self.dino_jump:
             self.jump()
         if self.dino_duck:
@@ -28,25 +29,32 @@ class Dinosaur(Sprite):
         if self.dino_run:
             self.run()
 
+        # and not self.dino_jump da prioridad a evitar a hacer multiples acciones
+        # Aqui se hacen las comprobaciones del teclado
         if user_input[pygame.K_DOWN] and not self.dino_jump:
             self.dino_run = False
             self.dino_duck = True
             self.dino_jump = False
+            
         elif user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_run = False
             self.dino_duck = False
             self.dino_jump = True
+            
+            # Si no toca nada entonces vuelve a correr
         elif not self.dino_jump:
             self.dino_run = True
             self.dino_duck = False
             self.dino_jump = False
-
+        
+        # Resetea la animacion en 0 para asegurar que se muestre todo correctamente
         if self.step_index >= 10:
             self.step_index = 0
 
     def draw(self, screen):
         screen.blit(self.image, self.dino_rect)
 
+    #Todos estos se llaman al update
     def run(self):
         self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
         self.dino_rect = self.image.get_rect()
@@ -62,9 +70,11 @@ class Dinosaur(Sprite):
 
     def jump(self):
         self.image = JUMPING
+        # Hace referencia para ser llamado arriba
         if self.dino_jump:
             self.dino_rect.y -= self.jump_vel * 4 # Salto
             self.jump_vel -= 0.8 # Salto, cuando llega a negativo, baja
+        # Ejecuta la logica para bajar al dinosario otra vez
         if self.jump_vel < -self.JUMP_VEL: # Cuando llega a JUMP_VEL en negativo, este se detiene
             self.dino_rect.y = self.POS_Y
             self.dino_jump = False
